d6628d58f7e2c41e7d60c2e767da1bc387cec630
ViniciusFBatista <viniciusfrancesquini03@gmail.com>
2024-06-01 10:06:42 -0300

    ControllerALUNO

--- src/controller/AlunoController.java
+++ src/controller/AlunoController.java
@@ -1,4 +1,9 @@
 package controller;
+
+import java.util.List;
+
+import model.Aluno;
+
 /**
  * Este Contrler Gerencia a lista de Alunos cadastrados
  * Versão: 1.0
@@ -6,6 +11,56 @@ package controller;
  * Data de criação: [28/05/2024]
  */
 
-public class AlunoController {
+ public class AlunoController {
+    private List<Aluno> alunos;
+
+    public AlunoController(List<Aluno> alunos) {
+        this.alunos = alunos;
+    }
+
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public void setTrienos(List<Aluno> alunos) {
+        this.alunos = alunos;
+    }
+
+    @Override
+    public String toString() {
+        return "AlunoContrller [alunos=" + alunos + "]";
+    }
+
+    public String cadastrarAluno(Aluno aluno) {
+        if (buscaAlunoId(aluno.getId() ) == null) {
+            aluno.setId(criarID());
+            alunos.add(aluno);
+            return "Aluno Cadastrado!";
+        }
+        return "Aluno Já Existe!";
+    }
+
+    public void deletarAluno(int id){
+        alunos.removeIf( t -> t.getId() == id);
+    }
+
+    public void limparAluno(){
+        alunos.clear();
+    }
+
+    public Aluno buscaAlunoId(int id) {
+        return alunos.stream().filter(c -> c.getId() == id).findFirst().orElse(null);
+    }
+
+    public int criarID() {
+        int res = 0;
+        for (Aluno p : alunos) {
+            if (p.getId() > res)
+                res = p.getId();
+        }
+        return ++res;
+    }
+    
+
     
 }
