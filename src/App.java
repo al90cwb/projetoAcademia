24c1bbfa4a377f714bea08a84cdf598eb8483eef
al90cwb <162759789+al90cwb@users.noreply.github.com>
2024-06-21 18:29:30 -0300

    doc/autores

--- src/App.java
+++ src/App.java
@@ -1,15 +1,15 @@
 /**
  * App principal Trabalho de java - tema academia
  * 
- * @author Alan Souza
- * @author Juan         35369671
- * @author Vinicius     30001331
- * @author Leonardo     
+ * @author Alan Souza 37883348
+ * @author Juan 35369671
+ * @author Vinicius 30001331
+ * @author Leonardo 28945565
  * @version 1.00
  * @since 11/06/2024
  */
 
- public class App {
+public class App {
     public static void main(String[] args) throws Exception {
         view.AcademiaView.main(args);
     }
